43f825b015790c4cd614e72045c77676085d3f5f
Karla Goo <karla_goo@yahoo.com>
2021-10-10 18:51:47 -0500

    created index model and fixed html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,16 +2,19 @@ const path = require("path");
 const router = require("./api-routes");
 const route = require("express").Router();
 
+// home route
 router.get("/", (req, res)=>{
-    res.sendFile(path.join(_dirname, "../public/index.html"))
+    res.sendFile(path.join(__dirname, "../public/index.html"))
 });
 
+// exercise route
 router.get("/exercise", (req, res)=>{
-    res.sendFile(path.join(_dirname, "../public/exercise.html"))
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
 });
 
+// stats route
 router.get("/stats", (req, res)=>{
-    res.sendFile(path.join(_dirname, "../public/stats.html"))
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
 });
 
 
