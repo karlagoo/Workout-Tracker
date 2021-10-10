c84de42a45d2ee286670e2801e950153f74d55e6
Karla Goo <karla_goo@yahoo.com>
2021-10-10 16:35:47 -0500

    created server.js, models and routes folders

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+const db = require("./models");
+
+// routes
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
