97c6e2462884ffb86721bd5e15df3f4eebeecfe2
Karla Goo <karla_goo@yahoo.com>
2021-10-10 18:16:32 -0500

    added html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,3 +1,18 @@
 const path = require("path");
+const router = require("./api-routes");
 const route = require("express").Router();
 
+router.get("/", (req, res)=>{
+    res.sendFile(path.join(_dirname, "../public/index.html"))
+});
+
+router.get("/exercise", (req, res)=>{
+    res.sendFile(path.join(_dirname, "../public/exercise.html"))
+});
+
+router.get("/stats", (req, res)=>{
+    res.sendFile(path.join(_dirname, "../public/stats.html"))
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 // require models
-const db = require("./models");
+const db = require("./models/workout");
 
 // require routes
 app.use(require("./routes/api-routes.js"));
