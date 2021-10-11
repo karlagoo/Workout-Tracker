a21585024006630c1c4cf1ed99d5decf6c0b1d4c
Karla Goo <karla_goo@yahoo.com>
2021-10-10 20:19:43 -0500

    created put route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,7 +13,7 @@ router.get("/api/workouts", (req, res) =>{
 });
 
 // creating a new workout
-router.post("/api/workouts", ({ body }, res) => {
+router.post("/api/workouts", ({body}, res) => {
     Workout.create(body)
       .then(dbWorkout => {
         res.json(dbWorkout);
@@ -22,5 +22,18 @@ router.post("/api/workouts", ({ body }, res) => {
         res.status(400).json(err);
       });
   });
+
+// editing a workout - ask tutor about this one
+router.put("api/workouts/:id", (req, res) =>{
+    Workout.findOneAndUpdate(
+        {_id: req.params.id},
+        {$push: {exercises: req.body}}
+    ).then(dbWorkout =>{
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+      });
+})
 
   module.exports = router;
